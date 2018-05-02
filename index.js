399bc543700cad9d73296ad4620d640a4cc09ae9
Jennifer Dobak <jdobak1@gmail.com>
2018-05-02 01:51:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ function scuberGreetingForFeet(someValue) {
   let greeting
   if (someValue <= 400) {
     greeting = 'This one is on me!'
-  } else if (someValue > 2000) {
-    greeting = 'I will gladly take your thirty bucks.'
   } else if (someValue > 2500) {
     greeting = 'No can do.'
-  }
+  } else if (someValue > 2000) {
+    greeting = 'I will gladly take your thirty bucks.'
+  } 
   return greeting
 }
